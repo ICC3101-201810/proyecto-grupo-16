0a04378f05c847b9f2ad7a742fd75dc235939112
Andres Howard <ahowardm@gmail.com>
2018-04-19 23:45:08 -0300

    Ordenar codigo

--- Proyecto-programacion-orientada-objetos/Proyecto-programacion-orientada-objetos/Program.cs
+++ Proyecto-programacion-orientada-objetos/Proyecto-programacion-orientada-objetos/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Entrega_2
 {
@@ -11,6 +8,7 @@ namespace Entrega_2
 
     static void Main(string[] args)
     {
+      Console.BackgroundColor = ConsoleColor.Black;
       ConsoleColor color = Console.ForegroundColor;
       Console.ForegroundColor = ConsoleColor.Red;
       Console.WriteLine("\tMódulo de talleres de la Universiad de los Andes\n");
